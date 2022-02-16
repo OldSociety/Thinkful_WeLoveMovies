b40692aac85b3fcc2bc4e32cf20a72864e215e83
OldSociety <headmistrust@outlook.com>
2022-02-16 11:56:36 -0800

    hopefully works

--- src/server.js
+++ src/server.js
@@ -1,16 +1,7 @@
-const express = require('express')
-const cors = require('cors')
-
 const { PORT = 5000 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
-
-router.get('/', cors(), (req, res) => {
-  res.json({ message: 'Hello Heroku!' });
-})
-
-app.use('/', router);
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
